6e13eaf3adb144d8d555d77c8256b9b66681fe82
Larry N <larryn35@gmail.com>
2020-11-04 17:53:42 -0800

    fetch and store data, create/join chatrooms

--- HelloWorld/HelloWorld/Views/ChatList.swift
+++ HelloWorld/HelloWorld/Views/ChatList.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct ChatList: View {
+    @ObservedObject var chatroomsViewModel = ChatroomsViewModel()
+    @State var joinModal = false
+    
+    init() {
+        chatroomsViewModel.fetchData()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(chatroomsViewModel.chatrooms) { chatroom in
+                HStack {
+                    Text(chatroom.title)
+                    Spacer()
+                }
+                .navigationBarTitle("Welcome")
+                .navigationBarItems(trailing: Button(action: {
+                    joinModal = true
+                }, label: {
+                    Image(systemName: "plus.circle")
+                }))
+                .sheet(isPresented: $joinModal, content: {
+                    Join(isOpen: $joinModal)
+                })
+            }
+        }
     }
 }
 
---
+++ HelloWorld/HelloWorld/Views/Join.swift
@@ -0,0 +1,55 @@
+//
+//  Join.swift
+//  HelloWorld
+//
+//  Created by Larry N on 11/4/20.
+//
+
+import SwiftUI
+
+struct Join: View {
+    
+    @Binding var isOpen: Bool
+    @State var joinCode = ""
+    @State var newTitle = ""
+    @ObservedObject var chatroomsViewModel = ChatroomsViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Join a chatroom")
+                        .font(.title)
+                    TextField("Enter your join code", text: $joinCode)
+                    Button(action: {
+                        chatroomsViewModel.joinChatroom(code: joinCode) {
+                            isOpen = false
+                        }
+                    }, label: {
+                        Text("Join")
+                    })
+                }
+                
+                VStack {
+                    Text("Create a chatroom")
+                        .font(.title)
+                    TextField("Enter a title", text: $newTitle)
+                    Button(action: {
+                        chatroomsViewModel.createChatroom(title: newTitle) {
+                            isOpen = false
+                        }
+                    }, label: {
+                        Text("Create")
+                    })
+                }
+            }
+            .navigationBarTitle("Join or create")
+        }
+    }
+}
+
+struct Join_Previews: PreviewProvider {
+    static var previews: some View {
+        Join(isOpen: .constant(true))
+    }
+}
