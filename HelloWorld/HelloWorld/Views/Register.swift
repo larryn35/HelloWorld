e589021998ebde07a2080b1bd80ab78235136c9a
Larry N <larryn35@gmail.com>
2020-11-14 16:29:57 -0800

    login, register screens

--- HelloWorld/HelloWorld/Views/Register.swift
+++ HelloWorld/HelloWorld/Views/Register.swift
@@ -15,116 +15,122 @@ struct Register: View {
     @State private var lastName = ""
     @State private var showAlert = false
     @State private var isLoading = false
-    @State private var showImagePicker = false
-    @State private var imageData : Data = .init(count: 0)
     @State private var errorMessage = ""
-    @Binding var showRegistration : Bool
+    
+    @Binding var keyboardDisplayed: Bool
     
     private var completedForm: Bool {
         // returns true if all fields are filled
         formValidation(for: [email, password, firstName, lastName])
     }
-        
+    
     @ObservedObject var sessionStore = SessionStore()
     @ObservedObject var userProfile = UserProfileViewModel()
     
     var body: some View {
-        
-        NavigationView {
-            ZStack {
-                VStack {
-                    Button(action: {
-                        showImagePicker = true
-                        self.hideKeyboard()
-                        
-                    }, label: {
-                        if self.imageData.count == 0 {
-                            Image("DefaultProfilePicture")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .clipShape(Circle())
-                                .shadow(radius: 15)
-                                .overlay(Circle().stroke(Color.white, lineWidth: 8))
-                                .frame(width: 150, height: 150)
-                        }
-                        else{
-                            if let image = UIImage(data: imageData) {
-                                Image(uiImage: image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .clipShape(Circle())
-                                    .shadow(radius: 15)
-                                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
-                                    .frame(width: 150, height: 150)
+        ZStack {
+            VStack {
+                VStack(alignment: .leading) {
+                                        
+                    HStack(spacing: 15) {
+                        Image(systemName: "person")
+                            .frame(width:20)
+                        TextField("first name", text: $firstName)
+                            .onTapGesture {
+                                keyboardDisplayed = true
                             }
-                        }
-                    })
-                    .padding(.vertical, 30)
+                    }
+                    .padding(.vertical)
                     
-                    Group {
-                        TextField("First Name", text: $firstName)
-                        TextField("Last Name", text: $lastName)
-                        TextField("Enter your email", text: $email)
+                    Divider()
+                    
+                    HStack(spacing: 15) {
+                        Image(systemName: "person")
+                            .frame(width:20)
+                        TextField("last name", text: $lastName)
+                            .onTapGesture {
+                                keyboardDisplayed = true
+                            }
+                    }
+                    .padding(.vertical)
+                    
+                    Divider()
+                    
+                    HStack(spacing: 15) {
+                        Image(systemName: "envelope")
+                            .frame(width:20)
+                        TextField("email", text: $email)
                             .keyboardType(.emailAddress)
                             .autocapitalization(.none)
-                        SecureField("Create a password (6 characters or longer)", text: $password)
-                    }
-                    .padding(.horizontal)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
-                    Button(action: {
-                        
-                        isLoading.toggle()
-                        
-                        sessionStore.signUp(email: email, password: password) { success, error  in
-                            if success, error == nil {
-                                if self.imageData.count == 0 {
-                                    userProfile.createProfile(firstName: firstName, lastName: lastName, email: email, imageData: nil)
-                                } else {
-                                    userProfile.createProfile(firstName: firstName, lastName: lastName, email: email, imageData: imageData)
-                                }
-                                
-                                showRegistration = false
-                                
-                            } else {
-                                showAlert = true
-                                errorMessage = "\(String(describing: error!.localizedDescription))"
-                                isLoading.toggle()
+                            .onTapGesture {
+                                keyboardDisplayed = true
+                                self.hideKeyboard()
                             }
-                        }
-                        
-                    }, label: {
-                        Text("Sign up")
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(completedForm ? Color.blue : Color.gray)
-                            .cornerRadius(25)
-                    })
-                    .disabled(!completedForm)
+                    }
+                    .padding(.vertical)
                     
-                    Spacer()
+                    Divider()
+                    
+                    HStack(spacing: 15) {
+                        Image(systemName: "lock")
+                            .frame(width:20)
+                        SecureField("password (min 6 characters)", text: $password)
+                            .onTapGesture {
+                                keyboardDisplayed = true
+                            }
+                    }
+                    .padding(.vertical)
+                    
                 }
-                .sheet(isPresented: $showImagePicker) {
-                    ImagePicker(imageData: self.$imageData)
-                }
+                .padding(.horizontal, 20)
                 
-                if isLoading {
-                    Loading()
-                }
+                Button(action: {
+                    
+                    isLoading.toggle()
+                    
+                    sessionStore.signUp(email: email, password: password) { success, error  in
+                        if success, error == nil {
+                            userProfile.createProfile(firstName: firstName, lastName: lastName, email: email, imageData: nil)
+                            
+                        } else {
+                            showAlert = true
+                            errorMessage = "\(String(describing: error!.localizedDescription))"
+                            isLoading.toggle()
+                        }
+                    }
+                    
+                }, label: {
+                    Text("sign up")
+                        .padding(8)
+                        .frame(width: UIScreen.main.bounds.width - 150)
+                        .foregroundColor(.white)
+                        .background(completedForm ? Color.red : Color.gray)
+                        .cornerRadius(10)
+                        .shadow(color: Color(.black).opacity(0.3), radius: 4, x: 4, y: 4)
+                })
+                .offset(y: 35)
             }
+            .padding(.vertical)
+            .background(Color(.white).opacity(0.7).cornerRadius(10).shadow(color: Color(.black).opacity(0.3), radius: 4, x: 4, y: 4))
+            .frame(width: UIScreen.main.bounds.width - 50)
+            
+            if isLoading {
+                Loading()
+            }
+            
         }
         .alert(isPresented: $showAlert) {
-            Alert(title: Text("Alert"), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            Alert(title: Text("Please try again"), message: Text(errorMessage), dismissButton: .default(Text("OK")))
         }
     }
 }
 
 
-//struct Register_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Register()
-//    }
-//}
+struct Register_Previews: PreviewProvider {
+    static var previews: some View {
+        Register(keyboardDisplayed: .constant(false))
+    }
+}
 
 func formValidation(for strings: [String]) -> Bool {
     var passes = [Bool]()
@@ -132,7 +138,7 @@ func formValidation(for strings: [String]) -> Bool {
         // field does not pass validation (false) if is empty / contains just spaces
         passes.append(!string.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
     }
-
+    
     // if any of the fields fail, return false, otherwise true
     if passes.contains(false) {
         return false
